33b1f12b1ef06979e7a6588a4e4e2fd0e9c1b390
kevinraim <kevinraimolugo@gmail.com>
2026-06-28 18:52:40 -0300

    Add navigate to "historial" button

--- Android/Proyecto_sin_Vibecoding/app/src/main/java/com/soal4/mochilaantirrobo/MainActivity.kt
+++ Android/Proyecto_sin_Vibecoding/app/src/main/java/com/soal4/mochilaantirrobo/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -148,6 +152,10 @@ fun PantallaAjuste(navController: NavController? = null) {
             // Con navigate le indicas que vaya a esa otra pantalla
             OutlinedButton(onClick = { navController?.navigate("info") }) {
                 Text("Ir a pantalla info")
+            }
+
+            OutlinedButton(onClick = { navController?.navigate("notif") }) {
+                Text("Ver historial de alertas")
             }
         }
     }
